42e3663001f3e8a6f5050a0e1cd387f7a84c7a86
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-11-09 11:50:08 -0300

    feat: add page in routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle.js";
 import Login from "./pages/Login";
+import NewProject from "./pages/NewProject/index.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/newProject" element={<NewProject />} />
       </Routes>
     </BrowserRouter>
   );
