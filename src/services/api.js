6fd0494bca2fdaf8a97b85e346083bfc2f0f5601
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2022-12-21 12:29:37 -0300

    fix: allow cors

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
+api.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+api.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
 export default Api;
